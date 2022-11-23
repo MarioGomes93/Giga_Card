417e18b661ccff28a57bb3a63c7cbf92856751da
Mario Gomes <mariodebora2@gmail.com>
2022-11-23 09:33:09 -0300

    Novas alterações para ser capaz de produzir documento pdf

---
+++ GigaCard/Controllers/GeraPDFController.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using GigaCard.Models;
+using Rotativa.AspNetCore;
+
+
+
+namespace AspCore_PDF.Controllers;
+
+public class GeraPDFController : Controller
+{
+    public IActionResult VisualizarComoPDF() => new ViewAsPdf("Card");
+}
