2f4ae2d187efa252199ff3c9ae40c1dee14fd78e
Mario Gomes <mariodebora2@gmail.com>
2022-11-28 15:52:09 -0300

    Configuração inicial para fazer download

--- GigaCard/wwwroot/js/site.js
+++ GigaCard/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function downloadPDF() {
+    const item = document.querySelector(".Content");
+
+    var opt = {
+        margin: 1,
+        filename: "meucard.pdf",
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+    };
+
+    html2pdf().set(opt).from(item).save(src ="~/Área de Trabalho/Teste");
+}
